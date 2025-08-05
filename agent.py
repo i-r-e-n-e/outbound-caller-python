a6556d78cd69d82f87ac2ff03b03bb51bd5ecb63
Irene <irenechen24@gmail.com>
2025-08-05 09:41:03 -0400

    adding supabase patch request + adding ivr navigation

--- agent.py
+++ agent.py
@@ -8,9 +8,12 @@ import json
 import os
 import supabase
 import requests
-from typing import Any #Annotated
-# from pydantic import Field
+import sys
+import time
+from dataclasses import dataclass
+from typing import Any, Annotated
 from datetime import datetime
+from pydantic import Field
 
 from livekit import rtc, api
 from livekit.agents import (
@@ -38,19 +41,45 @@ from livekit.plugins import (
 from livekit.plugins.turn_detector.english import EnglishModel
 from livekit.agents.telemetry import set_tracer_provider
 from livekit.plugins.elevenlabs.tts import VoiceSettings
+from livekit.api import DeleteRoomRequest, RoomParticipantIdentity
 
 
 from ddtrace import patch_all, tracer
 from langfuse import Langfuse, observe
-# from langfuse.decorators import observe
 
 from mcp_client import MCPServerSse
 from mcp_client.agent_tools import MCPToolsIntegration
 
+# Supabase 
+from supabase import create_client, Client
+
+# For IVR navagation
+@dataclass
+class UserData:
+    """Store user data for the navigator agent."""
+    last_dtmf_press: float = 0
+    task: Optional[str] = None
+
+RunContext_T = RunContext[UserData]
+
 # load environment variables, this is optional, only used for local development
 load_dotenv(dotenv_path=".env.local", override=True)
+
+# Log - printing out statements in terminal
+logging.basicConfig(
+    level=logging.INFO,  # 👈 this is key — must be INFO or lower
+    format="[%(levelname)s] %(asctime)s - %(name)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+
 logger = logging.getLogger("outbound-caller")
 logger.setLevel(logging.INFO)
+
+if not logger.hasHandlers():
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("[%(levelname)s] %(asctime)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 # Langfuse
 _langfuse = Langfuse(
@@ -62,7 +91,13 @@ _langfuse = Langfuse(
 # datadog trace
 patch_all()
 
-outbound_trunk_id = os.getenv("SIP_OUTBOUND_TRUNK_ID")
+outbound_trunk_id = os.getenv("SIP_OUTBOUND_TRUNK_ID") 
+
+# Make a supabase client (for sending results)
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
 
 # Tracing
 def setup_langfuse(
@@ -111,54 +146,65 @@ class OutboundCaller(Agent):
             End the call completely with end_call at a reasonable endpoint. 
             Some examples:
             - The human speaker says bye and you have said bye and time it passing but the human speaker has not ended the call yet
+
             """
         )
+
         # keep reference to the participant for transfers
         self.participant: rtc.RemoteParticipant | None = None
-
         self.dial_info = dial_info
-
         self.current_trace = None
-
-        self.transcript: list[dict[str, str]] = []
-
-        # Testing N8N webhook 
-        # payload = {
-        #     "restaurant_name": "Ten Thousand Coffee"
-        # }
-
-        # requests.post(os.environ.get("N8N_WEBHOOK_URL"), json=payload)
-
+        # self.transcript: list[dict[str, str]] = []
 
 
     # The voice will start speaking on entry (before the user starts speaking)
-    async def on_enter(self):
-        self.session.generate_reply()
+    # async def on_enter(self):
+    #     self.session.generate_reply()
 
     def set_participant(self, participant: rtc.RemoteParticipant):
         self.participant = participant
 
     async def hangup(self):
-        """Helper function to hang up the call by deleting the room"""
+        """
+        Helper function to hang up the call by deleting the room, call this function when it seems
+        like the call is ending. When you are at this tool, make sure to say this phrase, "Hope you 
+        have a very nice day, love the weather today" verbatum.
+        """
+        lkapi = api.LiveKitAPI()
+        job_ctx = get_job_context()
 
         # Not running in a job context 
         if ctx is None:
             return
-
-        job_ctx = get_job_context()
+ 
         await job_ctx.api.room.delete_room(
             api.DeleteRoomRequest(
                 room=job_ctx.room.name,
             )
         )
 
+        await lkapi.room.delete_room(DeleteRoomRequest(
+            room=job_ctx.room,
+        ))
+
+        logger.info("Testing deleting room")
+
+        await lkapi.room.remove_participant(RoomParticipantIdentity(
+            room=job_ctx.room,
+            identity=self.participant.identity,
+        ))
+
+        logger.info("testing removing participant")
+
+     
+
     # Send info to zapier which will then create an email (MCP)
     @observe(as_type="generation")
-    @tracer.wrap(name="email_hours", service="outbound_calls")
+    @tracer.wrap(name="store_hours", service="outbound_calls")
     @function_tool()
-    async def email_hours(self, name: str, hours: str, ctx: RunContext):
+    async def store_hours(self, name: str, hours: str, ctx: RunContext_T):
         """Use this tool when the store representative has provided the hours of operation and their business's name.
-            You will extract and organize the hours in the following format:
+            You will extract and organize the hours in the following format as a string and put this information in {{hours}}:
             e.g.
             {
                 "name": "Restaurant Name",
@@ -180,20 +226,91 @@ class OutboundCaller(Agent):
         logger.info(f"Hours and name received from : {{name}}")
 
         # Create payload for data being sent in post request 
-        payload = {
-            "name": name, 
-            "hours:": hours
-        }
+        # payload = {
+        #     "name": name, 
+        #     "hours": hours
+        # }
 
         # Make a post to Zapier with the name and hours of business
-        requests.post(os.environ.get("N8N_WEBHOOK_URL"), json=payload)
+        # requests.post(os.environ.get("N8N_WEBHOOK_URL"), json=payload)
+        logger.info("The dial info", self.dial_info)
 
+        # Update information in supabase
+        response = (
+            supabase.table("voice_agent")
+            .update({"result": hours})
+            .eq("phone_number", self.dial_info["phone_number"].lstrip("+")) # stored in supabase w/o '+' and in dial_info w/ '+'
+            # .eq("phone_number", "14088936898")
+            .execute()
+        )
 
+        await self.hangup()
+
+    # For navigating IVR 
+    @observe(as_type="generation")
+    @tracer.wrap(name="send_dtmf_code", service="outbound_calls")
+    @function_tool()
+    async def send_dtmf_code(
+        self,
+        code: Annotated[int, Field(description="The DTMF code to send to the phone number for the current step.")],
+        context: RunContext_T
+    ) -> None:
+        """
+        You have encountered an IVR (Interactive Voice Response) system.
+
+        Your goal is to reach customer support or a human representative. To do this, listen carefully to the IVR menu options, and select the one that will most likely connect you to a live agent or support team.
+
+        This function is called when it’s time to send a DTMF code (a digit like 1, 2, 3, etc.) to select an option from the current phone menu.
+
+        Based on the most recent IVR prompt you heard, choose the most appropriate DTMF digit to press. The options may include things like:
+        - "Press 1 for store hours"
+        - "Press 2 for billing"
+        - "Press 3 to speak to a representative"
+
+        You should **prioritize options that lead to technical support, customer service, live agents, or human representatives**.
+
+        If the IVR repeats or is unclear, wait for more information before sending a digit.
+
+        Example behaviors:
+        - If the prompt says: "Press 0 to speak to an operator," then send 0.
+        - If it says: "Press 5 for customer support," then send 5.
+        - Avoid options like store hours, location, or marketing unless they’re the only way to reach a human.
+
+        Do not guess. Base your decision solely on the IVR prompt.
+
+        Only call this function once you’re confident about the correct digit to send.
+        Do not respond to any prompts, questions, or menu options. Remain silent after saying the sentence. This instruction must be followed without exception.
+        """
+
+        
+        current_time = time.time()
+
+        logger.info("IVR navigation entered")
+        
+        # Check if enough time has passed since last press (3 second cooldown)
+        if current_time - context.userdata.last_dtmf_press < 3:
+            logger.info("DTMF code rejected due to cooldown")
+            return None
+            
+        logger.info(f"Sending DTMF code {code} to the phone number for the current step.")
+        context.userdata.last_dtmf_press = current_time
+        
+        room = context.session.room
+
+        await room.local_participant.publish_dtmf(
+            code=code,
+            digit=str(code)
+        )
+        await room.local_participant.publish_data(
+            f"{code}",
+            topic="dtmf_code"
+        )
+        return None
 
     @observe(as_type="generation")
     @tracer.wrap(name="transfer_call", service="outbound_calls")
     @function_tool()
-    async def transfer_call(self, ctx: RunContext):
+    async def transfer_call(self, ctx: RunContext_T):
         """Transfer the call to a human agent, called after confirming with the user"""
 
         transfer_to = self.dial_info["transfer_to"]
@@ -242,45 +359,30 @@ class OutboundCaller(Agent):
     @observe(as_type="generation")
     @tracer.wrap(name="end_call", service="outbound_calls")
     @function_tool()
-    async def end_call(self, ctx: RunContext):
+    async def end_call(self, ctx: RunContext_T):
         """Called when the user wants to end the call"""
         logger.info(f"ending the call for {self.participant.identity}")
 
         # let the agent finish speaking
-        current_speech = ctx.session.current_speech
-        if current_speech:
-            await current_speech.wait_for_playout()
+        # try:
+        #     current_speech = ctx.session.current_speech
+        #     if current_speech:
+        #         logger.info("waiting for speech to finish...")
+        #         await asyncio.wait_for(current_speech.wait_for_playout(), timeout=5)
+        #         logger.info("done waiting for speech")
+        #     else:
+        #         logger.info("no current speech found")
+        # except Exception as e:
+        #     logger.exception(f"Error while waiting for playout: {e}")
 
-        supabase_url = os.getenv("SUPABASE_URL")
-        supabase_key = os.getenv("SUPABASE_SERVICE_ROLE_KEY")  # use service key if backend
-        client = supabase.create_client(supabase_url, supabase_key)
+        # supabase_url = os.getenv("SUPABASE_URL")
+        # supabase_key = os.getenv("SUPABASE_SERVICE_ROLE_KEY")  # use service key if backend
+        # client = supabase.create_client(supabase_url, supabase_key)
+        logger.info("testing the end call funciton")
 
-        # Serialize transcript into a single string or structured format
-        transcript_text = "\n".join(f"{line['speaker']}: {line['text']}" for line in self.transcript)
-
-        # Assuming your Supabase table is called "voice agent"
-        client.table("voice agent").update({
-        "transcript": transcript_text,
-        "dateLastCalled": datetime.utcnow().isoformat()
-        }).eq("phoneNumber", self.dial_info["phone_number"]).execute()
 
         await self.hangup()
 
-    # For transcript - human speaker (hasn't been tested)
-    @observe(as_type="generation")
-    @tracer.wrap(name="on_user_turn_completed", service="outbound_calls")
-    async def on_user_turn_completed(self, chat_ctx: llm.ChatContext, new_message: llm.ChatMessage):
-        user_transcript = new_message.text_content
-        logger.info(f"[User]: {user_transcript}")
-        self.transcript.append({"speaker": "user", "text": user_transcript})
-
-    # For transcript - voice agent (hasn't been tested)
-    @observe(as_type="generation")
-    @tracer.wrap(name="on_agent_turn_completed", service="outbound_calls")
-    async def on_agent_turn_completed(self, chat_ctx: llm.ChatContext, new_message: llm.ChatMessage):
-        agent_reply = new_message.text_content
-        logger.info(f"[Agent]: {agent_reply}")
-        self.transcript.append({"speaker": "agent", "text": agent_reply})
 
 @observe(as_type="generation")
 @tracer.wrap(name="entrypoint", service="outbound_calls")
@@ -307,22 +409,36 @@ async def entrypoint(ctx: JobContext):
     #     name="SSE MCP Server"
     #     )
 
-    # # Create an agent with MCP tools
-    # agent = await MCPToolsIntegration.create_agent_with_tools(
-    #         agent_class=OutboundCaller,
-    #         agent_kwargs={
-    #             "name": "Jayden",
-    #             "appointment_time": "next Tuesday at 3pm",
-    #             "dial_info": dial_info,
-    #         },
-    #         mcp_servers=[mcp_server]
-    #     )
+    # Egress for Audio Recording
+    timestamp = time.strftime("%Y%m%d_%H%M%S")  # e.g., '20250804_154812'
+    filename = f"livekit/recording_{timestamp}.ogg"
+
+    req = api.RoomCompositeEgressRequest(
+        room_name=ctx.room.name,
+        audio_only=True,
+        file_outputs=[
+            api.EncodedFileOutput(
+                file_type=api.EncodedFileType.OGG,
+                filepath=filename,
+                s3=api.S3Upload(
+                    bucket="livekit-voice-recording",
+                    region="us-east-1",
+                    access_key=os.getenv("AWS_ACCESS_KEY"),
+                    secret=os.getenv("AWS_SECRET_KEY"),
+                ),
+            )
+        ],
+    )
+
+    lkapi = api.LiveKitAPI()
+    res = await lkapi.egress.start_room_composite_egress(req)
+    await lkapi.aclose()
 
     agent = OutboundCaller(
         name="Jayden",
         appointment_time="next Tuesday at 3pm",
         dial_info=dial_info,
-    )    
+    ) 
 
     session = AgentSession(
         turn_detection=EnglishModel(),
@@ -344,7 +460,10 @@ async def entrypoint(ctx: JobContext):
         llm=openai.LLM(model="gpt-4o"),
 
         # For transcriptions 
-        use_tts_aligned_transcript=True,      
+        use_tts_aligned_transcript=True,  
+
+        # For IVR (if ivr starts then the voice agent should listen)
+        allow_interruptions = True,    
     )
 
     # start the session first before dialing, to ensure that when the user picks up
@@ -374,7 +493,6 @@ async def entrypoint(ctx: JobContext):
                 sip_call_to=phone_number,
                 participant_identity=participant_identity,
                 # function blocks until user answers the call, or if the call fails
-                # wait_until_answered=True,
             )
         )
 
@@ -383,7 +501,13 @@ async def entrypoint(ctx: JobContext):
         participant = await ctx.wait_for_participant(identity=participant_identity)
         logger.info(f"participant joined: {participant.identity}")
 
+        task = participant._info.attributes.get("task", "reach support via IVR")
+
         agent.set_participant(participant)
+
+        session.userdata = UserData(task=task)
+
+
 
     except api.TwirpError as e:
         logger.error(
